ca4d6e6e0fbcb583624c14ffd710c7e4457aa48c
nik-markovic <nikola.m@gmail.com>
2025-11-26 16:53:19 -0600

    SDK integration checkpoint (compiles but crashes)

--- shared/include/ipc_communication.h
+++ shared/include/ipc_communication.h
@@ -60,40 +60,24 @@
 #define CY_IPC_MAX_ENDPOINTS            (5UL)
 #define CY_IPC_CYPIPE_CLIENT_CNT        (8UL)
 
-/*******************************************************************************
- * IPC1 Configuration for CM33 <-> CM55 Communication
- * 
- * PSE84 has two IPC instances:
- *   IPC0 (CPUSS_IPC):    Channels 0-15,  Interrupts 0-7  - Protected by TF-M
- *   IPC1 (APPCPUSS_IPC): Channels 16-31, Interrupts 8-15 - For user applications
- *
- * In a TF-M environment, IPC0 is protected by PPC and cannot be accessed
- * from the non-secure world. We must use IPC1 for CM33 NS <-> CM55 communication.
- *
- * Available IPC1 resources:
- *   - Channels: 17-30 (channel 16 reserved by PDL, channel 31 reserved for semaphores)
- *   - Interrupts: 9-15
- ******************************************************************************/
+#define CY_IPC_CHAN_CYPIPE_EP1          (4UL)
+#define CY_IPC_INTR_CYPIPE_EP1          (4UL)
+#define CY_IPC_CHAN_CYPIPE_EP2          (15UL)
+#define CY_IPC_INTR_CYPIPE_EP2          (5UL)
 
-/* IPC1 Channel and Interrupt assignments for custom pipe */
-#define CY_IPC_CHAN_CYPIPE_EP1          (18UL)   /* IPC1 channel 2 (PDL index 18) for CM33 */
-#define CY_IPC_INTR_CYPIPE_EP1          (9UL)    /* IPC1 interrupt 1 (PDL index 9) for CM33 */
-#define CY_IPC_CHAN_CYPIPE_EP2          (19UL)   /* IPC1 channel 3 (PDL index 19) for CM55 */
-#define CY_IPC_INTR_CYPIPE_EP2          (10UL)   /* IPC1 interrupt 2 (PDL index 10) for CM55 */
-
-/* IPC Pipe Endpoint-1 config (CM33) */
+/* IPC Pipe Endpoint-1 config */
 #define CY_IPC_CYPIPE_CHAN_MASK_EP1     CY_IPC_CH_MASK(CY_IPC_CHAN_CYPIPE_EP1)
 #define CY_IPC_CYPIPE_INTR_MASK_EP1     CY_IPC_INTR_MASK(CY_IPC_INTR_CYPIPE_EP1)
 #define CY_IPC_INTR_CYPIPE_PRIOR_EP1    (1UL)
-#define CY_IPC_INTR_CYPIPE_MUX_EP1      (CY_IPC1_INTR_MUX(CY_IPC_INTR_CYPIPE_EP1))
+#define CY_IPC_INTR_CYPIPE_MUX_EP1      (CY_IPC0_INTR_MUX(CY_IPC_INTR_CYPIPE_EP1))
 #define CM33_IPC_PIPE_EP_ADDR           (1UL)
 #define CM33_IPC_PIPE_CLIENT_ID         (3UL)
 
-/* IPC Pipe Endpoint-2 config (CM55) */
+/* IPC Pipe Endpoint-2 config */
 #define CY_IPC_CYPIPE_CHAN_MASK_EP2     CY_IPC_CH_MASK(CY_IPC_CHAN_CYPIPE_EP2)
 #define CY_IPC_CYPIPE_INTR_MASK_EP2     CY_IPC_INTR_MASK(CY_IPC_INTR_CYPIPE_EP2)
 #define CY_IPC_INTR_CYPIPE_PRIOR_EP2    (1UL)
-#define CY_IPC_INTR_CYPIPE_MUX_EP2      (CY_IPC1_INTR_MUX(CY_IPC_INTR_CYPIPE_EP2))
+#define CY_IPC_INTR_CYPIPE_MUX_EP2      (CY_IPC0_INTR_MUX(CY_IPC_INTR_CYPIPE_EP2))
 #define CM55_IPC_PIPE_EP_ADDR           (2UL)
 #define CM55_IPC_PIPE_CLIENT_ID         (5UL)
 
